337c59ec7908d458301309c1482e0de25a39f7ee
Ravin Rau <ravinrau1@gmail.com>
2024-06-02 15:43:19 +0800

    respond with body

--- app/main.js
+++ app/main.js
@@ -4,17 +4,30 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
+    //Request
+    socket.on("data", (data) => {
         const request = data.toString();
-        if (request.startsWith('GET / ')) {
-
-            const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
-            socket.write(httpResponse);
-        } else {
-            const httpResponse = 'HTTP/1.1 404 Not Found\r\n\r\n';
-            socket.write(httpResponse);
+        console.log("Request: \n" + request);
+        const url = request.split(' ')[1];
+        if(url == "/"){
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        }else if(url.includes("/echo/")){
+            const content = url.split('/echo/')[1];
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
+        }else{
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
+    });
+    //Error Handling
+    socket.on("error", (e) => {
+        console.error("ERROR: " + e);
         socket.end();
+        socket.close();
+    });
+    //Closing
+    socket.on("close", () => {
+        socket.end();
+        server.close();
     });
 });
 
