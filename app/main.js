c7a1d7bdba37ce822bb1d7c664d16d1c486a78e9
Ravin Rau <ravinrau1@gmail.com>
2024-06-02 15:32:52 +0800

    extract url path

--- app/main.js
+++ app/main.js
@@ -4,11 +4,18 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-    socket.write("GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n");
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+    socket.on('data', (data) => {
+        const request = data.toString();
+        if (request.startsWith('GET / ')) {
+
+            const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
+            socket.write(httpResponse);
+        } else {
+            const httpResponse = 'HTTP/1.1 404 NOT FOUND\r\n\r\n';
+            socket.write(httpResponse);
+        }
+        socket.end();
+    });
 });
 
 server.listen(4221, "localhost");
